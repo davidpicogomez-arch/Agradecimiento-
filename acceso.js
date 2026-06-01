5084f2df6c5c8306a098c6e7e26c85b0ce502c3f
davidpicogomez-arch <david.pico.gomez@gmail.com>
2026-05-31 23:14:03 -0500

    Primera version del proyecto
    Proyecto de experiencias personales interactivas; Con 10 personas y multimedia integrado

---
+++ acceso.js
@@ -0,0 +1,37 @@
+function verificar()
+{
+    const clave = document.getElementById("clave").value;
+
+    if(clave === "mama2026")
+        window.location.href = "personas/mama.html";
+
+    else if(clave === "papa2026")
+        window.location.href = "personas/papa.html";
+
+    else if(clave === "ana2026")
+        window.location.href = "personas/ana.html";
+
+    else if(clave === "daniela2026")
+        window.location.href = "personas/daniela.html";
+
+    else if(clave === "guevara2026")
+        window.location.href = "personas/guevara.html";
+
+    else if(clave === "bocacho2026")
+        window.location.href = "personas/bocacho.html"
+
+    else if(clave === "camilo2026")
+        window.location.href = "personas/camilo.html"
+
+    else if(clave === "juanita2026")
+        window.location.href = "personas/juanita.html"
+
+   else if(clave === "davzay2026")
+        window.location.href = "personas/davzay.html"
+
+   else if(clave === "dafne2026")
+        window.location.href = "personas/dafne.html"
+
+    else
+        document.getElementById("error").innerText = "Clave incorrecta";
+}
